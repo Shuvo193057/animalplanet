baaad1f7536322c9bad6888ffc44549860657613
miskatulkarim <mishkatulkarim@gmail.com>
2022-11-06 12:51:09 +0600

    adoption

--- src/Adoption/Adoption.js
+++ src/Adoption/Adoption.js
@@ -4,12 +4,14 @@ import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import { styled } from '@mui/material/styles';
-
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import CardActions from '@mui/material/CardActions';
+import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import BullDog from '../Resources/bulldog1.jpg';
+import Corgi from '../Resources/Corgi1.jpg';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -26,29 +28,63 @@ const Adoption = () => {
     return (
         <Container id="container" maxWidth="false">
             <Grid id="grid" container spacing={1}>
-             
+
                 <Grid item xs={12}>
-                
+
                     <Item id="item">
-                    <h3><b>Cat Section</b></h3>
-                    
+                        <h3><b>Cat Section</b></h3>
+
                         <Card id="card" sx={{ maxWidth: 345 }}>
-                            {/* <CardMedia
-                    component="img"
-                    alt="Corgi1"
-                    height="140"
-                    image="../Resources/Corgi1.jpg"
-                />
-                */}
-                            <img src="../Resources/bulldog1.jpg" alt=" "></img>
+                            <CardMedia id="pet-img"
+                                component="img"
+                                alt="Corgi1"
+                                height="140"
+                                image={BullDog}
+                            />
                             <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    Pet
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
+                                <label>Pet</label>
+                                <p>
                                     Lizards are a widespread group of squamate reptiles, with over 6,000
                                     species, ranging across all continents except Antarctica
-                                </Typography>
+                                </p>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Add</Button>
+                                <Button size="small">buy</Button>
+                            </CardActions>
+                        </Card>
+                        <Card id="card" sx={{ maxWidth: 345 }}>
+                            <CardMedia id="pet-img"
+                                component="img"
+                                alt="Corgi1"
+                                height="140"
+                                image={BullDog}
+                            />
+                            <CardContent>
+                                <label>Pet</label>
+                                <p>
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </p>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Add</Button>
+                                <Button size="small">buy</Button>
+                            </CardActions>
+                        </Card>
+                        <Card id="card" sx={{ maxWidth: 345 }}>
+                            <CardMedia id="pet-img"
+                                component="img"
+                                alt="Corgi1"
+                                height="140"
+                                image={BullDog}
+                            />
+                            <CardContent>
+                                <label>Pet</label>
+                                <p>
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </p>
                             </CardContent>
                             <CardActions>
                                 <Button size="small">Add</Button>
@@ -58,14 +94,12 @@ const Adoption = () => {
 
                         <h3><b>Dog Section</b></h3>
                         <Card id="card" sx={{ maxWidth: 345 }}>
-                            {/* <CardMedia
-                    component="img"
-                    alt="Corgi1"
-                    height="140"
-                    image="../Resources/Corgi1.jpg"
-                />
-                */}
-                            <img src="../Resources/husky1.jpg" alt=" "></img>
+                            <CardMedia
+                                component="img"
+                                alt="Corgi1"
+                                height="140"
+                                image={Corgi}
+                            />
                             <CardContent>
                                 <Typography gutterBottom variant="h5" component="div">
                                     Pet
@@ -80,16 +114,52 @@ const Adoption = () => {
                                 <Button size="small">Learn More</Button>
                             </CardActions>
                         </Card>
+                        <Card id="card" sx={{ maxWidth: 345 }}>
+                            <CardMedia id="pet-img"
+                                component="img"
+                                alt="Corgi1"
+                                height="140"
+                                image={BullDog}
+                            />
+                            <CardContent>
+                                <label>Pet</label>
+                                <p>
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </p>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Add</Button>
+                                <Button size="small">buy</Button>
+                            </CardActions>
+                        </Card>
+                        <Card id="card" sx={{ maxWidth: 345 }}>
+                            <CardMedia id="pet-img"
+                                component="img"
+                                alt="Corgi1"
+                                height="140"
+                                image={BullDog}
+                            />
+                            <CardContent>
+                                <label>Pet</label>
+                                <p>
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                </p>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Add</Button>
+                                <Button size="small">buy</Button>
+                            </CardActions>
+                        </Card>
                         <h3><b>Bird Section</b></h3>
                         <Card id="card" sx={{ maxWidth: 345 }}>
-                            {/* <CardMedia
-                    component="img"
-                    alt="Corgi1"
-                    height="140"
-                    image="../Resources/Corgi1.jpg"
-                />
-                */}
-                            <img src="../Resources/bulldog1.jpg" alt=" "></img>
+                            <CardMedia
+                                component="img"
+                                alt="Corgi1"
+                                height="140"
+                                image={Corgi}
+                            />
                             <CardContent>
                                 <Typography gutterBottom variant="h5" component="div">
                                     Pet
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -13,20 +13,17 @@ const Header = () => {
             <Navbar id="navbar" bg="dark" variant="dark">
                 <Container id="contain">
                     {/* <Navbar.Brand href="#home">Home</Navbar.Brand> */}
-                    <Nav className="me-auto">
-                        <Nav.Link href="#adoption">Adoption</Nav.Link>
-                        <Nav.Link href="#food">Food</Nav.Link>
-                        <Nav.Link href="#accessories">Accessories</Nav.Link>
-                        <Nav.Link href="#training">Training</Nav.Link>
-                        <Nav.Link href="#medicare">Medicare</Nav.Link>
-                        <Nav.Link href="#register">Register</Nav.Link>
+                    <Nav id="navigation">
+                        <NavLink className="links" to="/adoption">Adoption</NavLink>
+                        <NavLink className="links" to="/food">Food</NavLink>
+                        <NavLink className="links" to="/accessories">Accessories</NavLink>
+                        <NavLink className="links" to="/training">Training</NavLink>
+                        <NavLink className="links" to="/medicare">Medicare</NavLink>
+                        <NavLink className="links" to="/register">Register</NavLink>
                     </Nav>
                 </Container>
             </Navbar>
-            <div>
-                <button id="reg">Register</button>
-
-            </div>
+           
             {/* <div>
                 <body style="background-color:#1c87c9;">
 
