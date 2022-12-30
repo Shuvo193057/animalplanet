b73e2768a45142ff7c9df4f6e478037aa0802f94
Shuvo193057 <babushuvo1999@gmail.com>
2022-12-31 02:16:20 +0600

    Cart code added

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,39 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 
-const Header = () => {
+const Header = (props) => {
+
+    const [show, setShow] = useState(true);
+
     return (
 
         <div>
             <Navbar id="navbar" bg="dark" variant="dark">
                 <Container id="contain">
                     <div id="logo">
-                    <NavLink className="links" to="/">Animal Planet</NavLink>
+                        <NavLink className="links" to="/">Animal Planet</NavLink>
                     </div>
                     <Nav id="navigation">
-                    
+
                         <NavLink className="links" to="/adoption">Adoption</NavLink>
                         <NavLink className="links" to="/food">Food</NavLink>
                         <NavLink className="links" to="/accessories">Accessories</NavLink>
                         <NavLink className="links" to="/training">Training</NavLink>
                         <NavLink className="links" to="/medicare">Medicare</NavLink>
                         <NavLink className="links" to="/register">Register</NavLink>
+                        <NavLink className="links" to="/addcart" setShow={setShow}><ShoppingCartIcon />{props.Cart.length}</NavLink>
+
                     </Nav>
                 </Container>
             </Navbar>
-   {/* <div>
-                <body style="background-color:#1c87c9;">
-
-                </body>
-            </div> */}
         </div>
 
-);
+    );
 };
 
 export default Header;
